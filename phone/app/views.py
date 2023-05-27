58d136e5ec72929b23fb7e660dc6635f197d914f
Glumov Andrey <gluman@yandex.ru>
2023-05-27 17:44:02 +0300

    27.05.2023 v1.1

--- phone/app/views.py
+++ phone/app/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def show_phones(request, phone):
-    # count = int(request.GET.get('servings', 1))
-    # recipe = calc(bludo, count)
-    # context = {'recipe': recipe}
-    return render(request, 'recipe.html', context)
+def index(request):
+    return render(request, 'base.html')
+
+def phones_catalog(request, phone):
+
+
+    context = {}
+    return render(request, 'catalog.html', context)
--- phone/phone/urls.py
+++ phone/phone/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import index, phones_catalog
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/<phone>/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
+    path('', index, name='index'),
+    path('catalog/<phone>/', phones_catalog),
 ]
