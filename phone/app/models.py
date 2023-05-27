7d5604e3535fef10a22fdb0fad61db31e8921ae4
Glumov Andrey <gluman@yandex.ru>
2023-05-27 19:35:27 +0300

    27.05.2023 v2

--- phone/app/models.py
+++ phone/app/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Phone(models.Model):
     name = models.CharField(max_length=50)
-    price = models.CharField(max_length=50)
-    image = models.CharField(max_length=50)
+    price = models.FloatField()
+    image = models.CharField()
     release_date = models.DateField()
     lte_exists = models.BooleanField()
     slug = models.CharField(max_length=50)
--- phone/app/views.py
+++ phone/app/views.py
@@ -1,12 +1,19 @@
+import csv
+
 from django.shortcuts import render
 
 # Create your views here.
+
+def read_csv():
+    with open('phones.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+           phone = Phone()
 
 def index(request):
     return render(request, 'base.html')
 
 def phones_catalog(request, phone):
-
 
     context = {}
     return render(request, 'catalog.html', context)
