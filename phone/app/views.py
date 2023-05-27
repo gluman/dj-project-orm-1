3328a8d9d58d8f8ad66f34e3bdf2a2d7f11435e8
Glumov Andrey <gluman@yandex.ru>
2023-05-27 20:04:58 +0300

    27.05.2023 1. Создание структуры БД - Готово. 2. Чтение файла - Готово. 3. Запись в БД - почти готово. 4. Обработка запроса и чтение из БД - В работе. 5.  Переда выборки в шаблон - В работе.

--- phone/app/views.py
+++ phone/app/views.py
@@ -2,13 +2,34 @@ import csv
 
 from django.shortcuts import render
 
+from app.models import Phone
 # Create your views here.
 
+def slugging(text):
+    text_list = text.lower().split(' ')
+    slug = '-'.join(text_list)
+    return slug
 def read_csv():
+    phones = Phone()
+
     with open('phones.csv', 'rb') as f:
-        reader = csv.reader(f)
+        line_count = 0
+        reader = csv.reader(f, delimiter=';')
         for row in reader:
-           phone = Phone()
+            if line_count == 0:
+               line_count += 1
+               continue
+            else:
+                phone = Phone(
+                    name=row[1],
+                    image=row[2],
+                    price=row[3],
+                    release_date=row[4],
+                    lte_exists=row[5],
+                    slug=slugging(row[1]),
+                )
+                phone.save()
+                line_count += 1
 
 def index(request):
     return render(request, 'base.html')
